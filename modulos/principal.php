d0aa5c163f752af07df383af00e86338c2a08d0b
Elton <eltonknoxville@hotmail.com>
2014-06-13 22:33:44 -0400

    Listagem Imagens SlideShow
    Listagem das imagens do slideShow

--- modulos/principal.php
+++ modulos/principal.php
@@ -134,8 +134,8 @@ $qryBanner = $db->query($strBanner);
 
                           }else{
 
-                               $queryquerySlideImage = "SELECT nmNomeArquivo,nmTituloArquivo,nmDescricaoArquivo FROM tb_arquivo WHERE idArquivo = ".$query[0]['idArquivo'];   
-                               $imagesSlide = $db->query($queryquerySlideImage);     
+                               $querySlideImage = "SELECT nmNomeArquivo,nmTituloArquivo,nmDescricaoArquivo FROM tb_arquivo WHERE idArquivo = ".$query[0]['idArquivo'];   
+                               $imagesSlide = $db->query($querySlideImage);     
                           } 
 
                           $countImage = count($imagesSlide);
@@ -147,11 +147,11 @@ $qryBanner = $db->query($strBanner);
                           /*LIST SLIDE*/
 
                             for($i = 0 ; $i <= $countImage;$i++){
-
-                              /*  echo '
-                                          <div class="panel" title="Panel '.($i+2).' ">
+                              
+                                echo '
+                                        <div class="panel" title="Panel '.($i+2).' ">
                                             <div class="wrapper">
-                                                <img src="http://www.ddchannel.com.br/image/'.$imagesSlide[$i]['nmNomeArquivo'].'" alt="temp" />
+                                                <img src="http://www.ddchannel.com.br/arquivos/enviados/image/'.$imagesSlide[$i]['nmNomeArquivo'].'" alt="temp" />
                                                 <div class="photo-meta-data">
                                                     Chicago Bears at Seattle Seahawks<br />
                                                     <span>Fifth field goal, overtime win for the Seahawks</span>
@@ -159,7 +159,7 @@ $qryBanner = $db->query($strBanner);
                                             </div>
                                         </div>
 
-                                    ';*/
+                                    ';
                             }
 
                           /*END LIST*/
@@ -168,48 +168,9 @@ $qryBanner = $db->query($strBanner);
             ?>
 
                     
-                 <div class="panel" title="Panel 3">
-                    <div class="wrapper">
-                        
-                        <img src="http://www.ddchannel.com.br/images_teste/scotch-egg.jpg" alt="scotch egg" class="floatLeft"/>
-                        
-                        <h1>How to Cook a Scotch Egg</h1>
-                        
-                        <ul>
-                            <li>6 hard-boiled eggs, well chilled (i try to cook them to just past soft boiled stage, then stick them in the coldest part of the fridge to firm up)</li>
-                            <li>1 pound good quality sausage meat (i used ground turkey meat, seasoned with sage, white pepper, salt and a tiny bit of maple syrup)</li>
-                            <li>1/2 cup AP flour</li>
-                            <li>1-2 eggs, beaten</li>
-                            <li>3/4 cup panko-style bread crumbs</li>
-                            <li>Vegetable oil for frying</li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="panel" title="Panel 4">
-                    <div class="wrapper">
-                        <img src="images/tempphoto-4.jpg" alt="temp" />
-                        <div class="photo-meta-data">
-                            A Poem by Shel Silverstein<br />
-                            <span>Falling Up</span>
-                        </div>
-                    </div>
-                </div>
-                <div class="panel" title="Panel 5">
-                    <div class="wrapper">
-                        <img src="http://www.ddchannel.com.br/images_teste/tempphoto-5.jpg" alt="temp" />
-                        <div class="photo-meta-data">
-                            New Video on CSS-Tricks<br />
-                            <span>Using Wufoo for Web Forms</span>
-                        </div>
-                    </div>
-                </div>
-                <div class="panel" title="Panel 6">
-                    <div class="wrapper">
-                        <h1>A Tale of Two Cities</h1>
-                        <p><em>Charles Dickins</em></p>
-                        <blockquote>It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to heaven, we were all going direct the other way - in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only.</blockquote>
-                    </div>
-                </div>
+               
+              
+               
 
             </div>
         </div>
